d26976ae985c12f280d12241ef231353e2f337d9
David Nimmo <dnimmo@gmail.com>
2024-07-21 03:48:53 +0100

    added chevrons

--- src/MG/Icons.elm
+++ src/MG/Icons.elm
@@ -1,11 +1,11 @@
-module MG.Icons exposing (show, hide, rightArrow, leftArrow, warning, success, logOut, cross, plus)
+module MG.Icons exposing (show, hide, rightArrow, leftArrow, warning, success, logOut, cross, plus, chevronDown, chevronUp)
 
 {-|
 
 
 # Icons
 
-@docs show, hide, rightArrow, leftArrow, warning, success, logOut, cross, plus
+@docs show, hide, rightArrow, leftArrow, warning, success, logOut, cross, plus, chevronDown, chevronUp
 
 -}
 
@@ -73,3 +73,28 @@ plus =
 cross : Int -> Element msg
 cross =
     icon Icons.close
+
+
+chevronRight : Int -> Element msg
+chevronRight =
+    icon Icons.chevron_right
+
+
+{-| -}
+chevronDown : Int -> Element msg
+chevronDown size =
+    let
+        chevron =
+            chevronRight size
+    in
+    el [ rotate <| degrees 90 ] chevron
+
+
+{-| -}
+chevronUp : Int -> Element msg
+chevronUp size =
+    let
+        chevron =
+            chevronRight size
+    in
+    el [ rotate <| degrees 270 ] chevron
